b97fc1d1a5396f19ca27686e4ab10a67372c5afe
David Vicklund <david@vicklund.com>
2015-11-28 14:34:08 -0800

    router: add get /

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.use(express.static('build'));
 
+app.get('/', function(req, res) {
+  res.redirect(300, 'index.html');
+})
+
 app.listen(process.env.PORT || 3000, function() {
   console.log('server listening');
 });
