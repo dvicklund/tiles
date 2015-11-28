27a5c7096f64c090667fbbd1845dc8f942952147
David Vicklund <david@vicklund.com>
2015-11-28 13:02:14 -0800

    one more index tweak (work plz)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/build'));
+app.use('/', express.static(__dirname + '/build'));
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('server listening');
