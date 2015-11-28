b87de7b06542b88077467ad5e77e74a86d78ded8
David Vicklund <david@vicklund.com>
2015-11-28 12:58:48 -0800

    index tweak

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('build'));
+app.use(express.static(__dirname + '/build'));
+
+app.get('/', function(req, res) {
+  res.redirect('/index.html');
+})
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('server listening');
