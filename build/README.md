d5f3660336fdb9d420b91b7712625b6c8fe96466
David Vicklund <david@vicklund.com>
2015-11-28 20:07:21 -0800

    add favicon.ico

--- build/README.md
+++
@@ -1,4 +0,0 @@
-[![Build Status](https://travis-ci.org/dvicklund/tiles.svg)](https://travis-ci.org/dvicklund/tiles)
-
-# tiles
-A simple, randomly generated, tile-based game
--- build/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static('build'));
-
-app.listen(process.env.PORT || 3000, function() {
-  console.log('server listening');
-});
